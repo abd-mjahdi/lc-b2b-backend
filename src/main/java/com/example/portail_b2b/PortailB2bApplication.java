4527091fd60f20c37bbf177cb89103b293a02f7d
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-07-13 10:14:24 +0100

    first commit

---
+++ src/main/java/com/example/portail_b2b/PortailB2bApplication.java
@@ -0,0 +1,13 @@
+package com.example.portail_b2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailB2bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailB2bApplication.class, args);
+	}
+
+}
